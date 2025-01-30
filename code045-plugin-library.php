58b7436d93bdbef5b64e805da8212eb135346578
Code045 <maurice@code045.nl>
2025-01-30 22:06:14 +0000

    reformat code

--- code045-plugin-library.php
+++ code045-plugin-library.php
@@ -6,8 +6,18 @@
  * Author: Code045
  */
 
+register_activation_hook(__FILE__, 'code045_activate');
 add_action('admin_menu', 'code045_add_admin_menu');
 add_action('admin_init', 'code045_settings_init');
+add_action('plugins_loaded', 'code045_load_manager');
+
+function code045_activate() {
+    $options = get_option('code045_settings');
+    if (!isset($options['code045_mode'])) {
+        $options['code045_mode'] = 'client';
+        update_option('code045_settings', $options);
+    }
+}
 
 function code045_add_admin_menu() {
     add_menu_page('Code045 Plugin Library', 'Code045 Plugin Library', 'manage_options', 'code045_plugin_library', 'code045_options_page');
@@ -16,29 +26,42 @@ function code045_add_admin_menu() {
 function code045_settings_init() {
     register_setting('code045_plugin_library', 'code045_settings');
 
-    add_settings_section(
-        'code045_plugin_library_section',
-        __('Settings', 'code045'),
-        'code045_settings_section_callback',
-        'code045_plugin_library'
-    );
+    add_settings_section('code045_plugin_library_section', __('Settings', 'code045'), 'code045_settings_section_callback', 'code045_plugin_library');
 
-    add_settings_field(
-        'code045_mode',
-        __('Mode', 'code045'),
-        'code045_mode_render',
-        'code045_plugin_library',
-        'code045_plugin_library_section'
-    );
+    add_settings_field('code045_mode', __('Mode', 'code045'), 'code045_mode_render', 'code045_plugin_library', 'code045_plugin_library_section');
+    add_settings_field('code045_server_api_key', __('Server API Key', 'code045'), 'code045_server_api_key_render', 'code045_plugin_library', 'code045_plugin_library_section');
+    add_settings_field('code045_client_server_url', __('Server URL', 'code045'), 'code045_client_server_url_render', 'code045_plugin_library', 'code045_plugin_library_section');
+    add_settings_field('code045_client_api_key', __('Client API Key', 'code045'), 'code045_client_api_key_render', 'code045_plugin_library', 'code045_plugin_library_section');
 }
 
 function code045_mode_render() {
     $options = get_option('code045_settings');
     ?>
-    <select name='code045_settings[code045_mode]'>
+    <select name='code045_settings[code045_mode]' id='code045_mode'>
         <option value='server' <?php selected($options['code045_mode'], 'server'); ?>>Server</option>
         <option value='client' <?php selected($options['code045_mode'], 'client'); ?>>Client</option>
     </select>
+    <?php
+}
+
+function code045_server_api_key_render() {
+    $options = get_option('code045_settings');
+    ?>
+    <input type='text' name='code045_settings[code045_server_api_key]' value='<?php echo $options['code045_server_api_key']; ?>' />
+    <?php
+}
+
+function code045_client_server_url_render() {
+    $options = get_option('code045_settings');
+    ?>
+    <input type='text' name='code045_settings[code045_client_server_url]' value='<?php echo $options['code045_client_server_url']; ?>' />
+    <?php
+}
+
+function code045_client_api_key_render() {
+    $options = get_option('code045_settings');
+    ?>
+    <input type='text' name='code045_settings[code045_client_api_key]' value='<?php echo $options['code045_client_api_key']; ?>' />
     <?php
 }
 
@@ -59,8 +82,6 @@ function code045_options_page() {
     <?php
 }
 
-add_action('plugins_loaded', 'code045_load_manager');
-
 function code045_load_manager() {
     $options = get_option('code045_settings');
     $mode = isset($options['code045_mode']) ? $options['code045_mode'] : 'client'; // Default to 'client' if not set
@@ -70,6 +91,6 @@ function code045_load_manager() {
         new Code045_Server_Manager();
     } else {
         require_once plugin_dir_path(__FILE__) . 'includes/code045-plugin-manager.php';
-        new Code045_Plugin_Manager();
+        new Code045_Client_Manager();
     }
 }
--- includes/code045-plugin-manager.php
+++ includes/code045-plugin-manager.php
@@ -1,103 +1,28 @@
 <?php
-class Code045_Plugin_Manager {
+class Code045_Client_Manager {
+    private $server_url;
+    private $api_key;
+
     public function __construct() {
-        add_action('rest_api_init', array($this, 'register_rest_routes'));
         add_action('admin_menu', array($this, 'add_admin_menu'));
+        $options = get_option('code045_settings');
+        $this->server_url = $options['code045_client_server_url'];
+        $this->api_key = $options['code045_client_api_key'];
     }
 
     public function add_admin_menu() {
-        add_menu_page(
-            'Code045 Plugin Manager',
-            'Code045 Plugins',
-            'manage_options',
-            'code045-plugin-manager',
-            array($this, 'render_admin_page'),
-            'dashicons-admin-plugins'
-        );
+        add_submenu_page('code045_plugin_library', 'Connected Plugins', 'Connected Plugins', 'manage_options', 'code045-connected-plugins', array($this, 'render_connected_plugins_page'));
     }
 
-    public function render_admin_page() {
+    public function render_connected_plugins_page() {
         ?>
         <div class="wrap">
-            <h1>Code045 Plugin Manager</h1>
-            <div id="code045-plugin-manager">
-                <div class="code045-status"></div>
-                <div class="code045-plugins-list"></div>
+            <h1>Connected Plugins</h1>
+            <div class="card">
+                <h2>Plugins on Server</h2>
+                <p>List of plugins installed on the connected server will be displayed here.</p>
             </div>
         </div>
         <?php
     }
-
-    public function register_rest_routes() {
-        register_rest_route('code045/v1', '/plugins/check/(?P<name>[a-zA-Z0-9-_]+)', array(
-            'methods' => 'GET',
-            'callback' => array($this, 'check_plugin_installation'),
-            'permission_callback' => function() {
-                return current_user_can('install_plugins');
-            }
-        ));
-
-        register_rest_route('code045/v1', '/plugins/install/(?P<id>[a-zA-Z0-9-]+)', array(
-            'methods' => 'POST',
-            'callback' => array($this, 'install_plugin'),
-            'permission_callback' => function() {
-                return current_user_can('install_plugins');
-            }
-        ));
-    }
-
-    public function check_plugin_installation($request) {
-        $plugin_name = $request['name'];
-        
-        if (!function_exists('get_plugins')) {
-            require_once ABSPATH . 'wp-admin/includes/plugin.php';
-        }
-        
-        $all_plugins = get_plugins();
-        $installed = false;
-
-        foreach ($all_plugins as $plugin_path => $plugin_data) {
-            if (strpos($plugin_path, $plugin_name) !== false) {
-                $installed = true;
-                break;
-            }
-        }
-
-        return new WP_REST_Response(array(
-            'installed' => $installed
-        ), 200);
-    }
-
-    public function install_plugin($request) {
-        require_once ABSPATH . 'wp-admin/includes/file.php';
-        require_once ABSPATH . 'wp-admin/includes/plugin-install.php';
-        require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
-        require_once ABSPATH . 'wp-admin/includes/plugin.php';
-
-        $plugin_id = $request['id'];
-        
-        try {
-            $upgrader = new Plugin_Upgrader(new WP_Ajax_Upgrader_Skin());
-            $installed = $upgrader->install($plugin_url);
-
-            if (is_wp_error($installed)) {
-                return new WP_REST_Response(array(
-                    'success' => false,
-                    'message' => $installed->get_error_message()
-                ), 500);
-            }
-
-            return new WP_REST_Response(array(
-                'success' => true,
-                'message' => 'Plugin installed successfully'
-            ), 200);
-        } catch (Exception $e) {
-            return new WP_REST_Response(array(
-                'success' => false,
-                'message' => $e->getMessage()
-            ), 500);
-        }
-    }
 }
-
-$code045_plugin_manager = new Code045_Plugin_Manager();
