d653bfb68dcb751824c928ba1cd4b452011c5953
Code045 <maurice@code045.nl>
2025-01-31 00:04:10 +0000

    reformat

--- code045-plugin-library/code045-plugin-library.php
+++ code045-plugin-library/code045-plugin-library.php
@@ -15,6 +15,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-code045-plugin-library.php';
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-code045-remote-connection.php';
 
+// Register settings
+function code045_register_settings() {
+    register_setting('code045_plugin_library_settings', 'code045_remote_url');
+    register_setting('code045_plugin_library_settings', 'code045_remote_username');
+    register_setting('code045_plugin_library_settings', 'code045_remote_password');
+}
+add_action('admin_init', 'code045_register_settings');
+
 // Initialize the plugin
 function code045_plugin_library_init() {
     $plugin_library = new Code045_Plugin_Library();
--- code045-plugin-library/includes/class-code045-plugin-library.php
+++ code045-plugin-library/includes/class-code045-plugin-library.php
@@ -1,27 +1,64 @@
 <?php
 class Code045_Plugin_Library {
-    public function __construct() {
+
+    public function init() {
         add_action('admin_menu', array($this, 'add_admin_pages'));
-        add_action('admin_init', array($this, 'settings_init'));
     }
 
     public function add_admin_pages() {
-        add_menu_page('Code045 Plugin Library', 'Plugin Library', 'manage_options', 'code045-plugin-library', array($this, 'settings_page'));
-        add_submenu_page('code045-plugin-library', 'Installed Plugins', 'Installed Plugins', 'manage_options', 'code045-plugins-list', array($this, 'plugins_list_page'));
+        add_menu_page(
+            'Code045 Plugin Library Settings',
+            'Code045 Plugin Library',
+            'manage_options',
+            'code045-plugin-library-settings',
+            array($this, 'settings_page_html'),
+            '',
+            100
+        );
+
+        add_submenu_page(
+            'code045-plugin-library-settings',
+            'Installed Plugins',
+            'Installed Plugins',
+            'manage_options',
+            'code045-plugin-library-plugins',
+            array($this, 'plugins_page_html')
+        );
     }
 
-    public function settings_page() {
-        include plugin_dir_path(__FILE__) . '../admin/partials/settings-page.php';
+    public function settings_page_html() {
+        ?>
+        <div class="wrap">
+            <h1>Code045 Plugin Library Settings</h1>
+            <form method="post" action="options.php">
+                <?php
+                settings_fields('code045_plugin_library_settings');
+                do_settings_sections('code045_plugin_library_settings');
+                ?>
+                <table class="form-table">
+                    <tr valign="top">
+                        <th scope="row">Remote URL</th>
+                        <td><input type="text" name="code045_remote_url" value="<?php echo esc_attr(get_option('code045_remote_url')); ?>" /></td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">Username</th>
+                        <td><input type="text" name="code045_remote_username" value="<?php echo esc_attr(get_option('code045_remote_username')); ?>" /></td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">Password</th>
+                        <td><input type="password" name="code045_remote_password" value="<?php echo esc_attr(get_option('code045_remote_password')); ?>" /></td>
+                    </tr>
+                </table>
+                <?php submit_button(); ?>
+            </form>
+        </div>
+        <?php
     }
 
-    public function plugins_list_page() {
-        include plugin_dir_path(__FILE__) . '../admin/partials/plugins-list-page.php';
-    }
-
-    public function settings_init() {
-        register_setting('code045_plugin_library', 'code045_remote_url');
-        register_setting('code045_plugin_library', 'code045_remote_username');
-        register_setting('code045_plugin_library', 'code045_remote_password');
+    public function plugins_page_html() {
+        // HTML for plugins list page
+        echo '<h1>Installed Plugins</h1>';
+        // Add logic to list plugins from remote system
     }
 }
 ?>
