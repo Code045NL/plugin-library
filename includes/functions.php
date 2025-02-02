ed5b7a62bb9ee2f00335ebf21b625054f832c732
Code045 <maurice@devtigspace.nl>
2025-02-02 07:06:53 +0000

    func

--- includes/functions.php
+++ includes/functions.php
@@ -17,28 +17,6 @@ function get_plugin_data_by_slug($plugin_slug) {
     return null;
 }
 
-function create_plugin_zip($plugin_dir, $backup_dir, $plugin_slug, $plugin_version) {
-    $zip = new ZipArchive();
-    $zip_file = $backup_dir . '/' . $plugin_slug . '-' . $plugin_version . '.zip';
-
-    if ($zip->open($zip_file, ZipArchive::CREATE | ZipArchive::OVERWRITE) === TRUE) {
-        $root_path = realpath($plugin_dir);
-        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($root_path), RecursiveIteratorIterator::LEAVES_ONLY);
-
-        foreach ($files as $name => $file) {
-            if (!$file->isDir()) {
-                $file_path = $file->getRealPath();
-                $relative_path = $plugin_slug . '/' . substr($file_path, strlen($root_path) + 1);
-                $zip->addFile($file_path, $relative_path);
-            }
-        }
-
-        $zip->close();
-    } else {
-        throw new Exception('Failed to create zip file.');
-    }
-}
-
 add_action('wp_enqueue_scripts', 'plugin_library_enqueue_styles');
 
 // Enqueue FontAwesome
