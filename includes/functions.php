f5b9160db7b8320cd084fdf46b5b5b0d1cea76d0
Code045 <maurice@devtigspace.nl>
2025-02-02 03:08:04 +0000

    changs

--- includes/functions.php
+++ includes/functions.php
@@ -3,7 +3,8 @@
 function get_plugin_data_by_slug($plugin_slug) {
     $plugins = get_plugins();
     foreach ($plugins as $plugin_file => $plugin_data) {
-        if (dirname($plugin_file) === $plugin_slug) {
+        $slug = dirname($plugin_file);
+        if ($slug === $plugin_slug) {
             $backup_dir = ABSPATH . 'plugin-library';
             $plugin_version = $plugin_data['Version'];
             $zip_file = $backup_dir . '/' . $plugin_slug . '-' . $plugin_version . '.zip';
